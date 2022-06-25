311a9465a6e051b955386c1387fea1e6c3e25642
wangkaiwd <1162486176@qq.com>
2022-06-25 18:05:31 +0800

    feat: tag name start with v

--- .release-it.js
+++ .release-it.js
@@ -1,6 +1,7 @@
 module.exports = {
   git: {
-    'commitMessage': 'chore: release v${version}'
+    commitMessage: 'chore: release v${version}',
+    tagName: 'v${version}'
   },
   npm: {
     publish: false
