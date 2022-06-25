68bb721c1ceee06dd1ea28ccbe88ad8e2712afef
wangkaiwd <1162486176@qq.com>
2022-06-25 17:53:58 +0800

    feat: record version in app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
 import HelloWorld from '@/components/HelloWorld.vue';
+import pkg from '../package.json';
 </script>
 
 <template>
@@ -8,8 +9,7 @@ import HelloWorld from '@/components/HelloWorld.vue';
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125"/>
 
     <div class="wrapper">
-      <HelloWorld msg="You did it! V1.0.0"/>
-
+      <HelloWorld :msg="`You did it!${pkg.version}`"/>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
